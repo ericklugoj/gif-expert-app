4a3a58ccf088a5502d6003f22186d888916dbcb6
Erick Lugo <ericklugoj@gmail.com>
2020-08-16 22:37:16 -0500

    Animaciones al cargar agregadas

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useFetchGifs from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
-import getGifs from '../helpers/getGifs';
 
 const GifGrid = ({ category }) => {
-	const [images, setImages] = useState([]);
-
-	useEffect(() => {
-		getGifs(category).then(setImages);
-	}, [category]);
+	const { data: images, loading } = useFetchGifs(category);
 
 	return (
 		<>
-			<h3>{category}</h3>
+			<h3 className='animate__animated animate__bounceIn'>{category}</h3>
+
+			{loading && <p>Loading...</p>}
+
 			<div className='card-grid'>
 				{images.map((img) => (
 					<GifGridItem key={img.id} {...img} />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import GifExpertApp from './GifExpertApp'
 import './index.css';
+import 'animate.css';
 
 ReactDOM.render(
   <GifExpertApp />,
