e1d79413385dd692b427bc84e36d5398b28f154b
Erick Lugo <ericklugoj@gmail.com>
2020-08-20 00:14:21 -0500

    Build desplegado

---
+++ docs/precache-manifest.67083cffd61c8c19dba35d71f2c0026e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3360aceaef53e4c5508987aa5ec383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "c0816fddf4dafa89d1d9",
+    "url": "/static/css/main.7dbd2fae.chunk.css"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/js/2.1a68ba2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a68ba2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0816fddf4dafa89d1d9",
+    "url": "/static/js/main.33ae6b83.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
